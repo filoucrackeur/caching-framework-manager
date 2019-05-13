a9c53e09f8099d6b5af20a7eda99d2d2ec43805b
Philippe  Court <philippe.court@gmail.com>
2019-05-13 21:36:01 +0200

    [BUGFIX] Fix error collapse core and extension same identifier

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,28 +1,28 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'Storage framework manager',
-    'description' => 'Storage framework manager extension for TYPO3',
-    'category' => 'module',
-    'state' => 'stable',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author' => 'Philippe Court',
-    'author_email' => 'contact@webstationservice.fr',
-    'author_company' => 'Web Station Service',
-    'version' => '1.0.0',
-    '_md5_values_when_last_written' => '',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'php' => '7.2.99-7.3.99',
-                    'typo3' => '9.5.99-9.9.99',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
-        ],
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Storage framework manager',
+  'description' => 'Storage framework manager extension for TYPO3',
+  'category' => 'module',
+  'state' => 'stable',
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'author' => 'Philippe Court',
+  'author_email' => 'contact@webstationservice.fr',
+  'author_company' => 'Web Station Service',
+  'version' => '1.0.1',
+  '_md5_values_when_last_written' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'php' => '7.2.99-7.3.99',
+      'typo3' => '9.5.99-9.9.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
